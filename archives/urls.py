339fba09d8ee4e95424b37d805251092392a1069
xjjwang <xjjwang@outlook.com>
2025-03-13 21:58:38 +0800

    fix bugs

--- archives/urls.py
+++ archives/urls.py
@@ -5,6 +5,8 @@ app_name = 'archives'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.archives_login, name='login'),
+    path('logout/', views.archives_logout, name='logout'),
     path('create-archive-box/', views.create_archive_box, name='create_archive_box'),
     path('archive-box/<int:pk>/', views.archive_box_detail, name='archive_box_detail'),
     path('add-archives/', views.add_archives, name='add_archives'),
